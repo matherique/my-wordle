f34c9f9d3bcbf9324f71dd3c2f57b5e119fc6a99
Matheus Henrique <matherique@gmail.com>
2022-03-01 12:13:18 -0300

    test: add more test

--- src/__test__/wordle.test.tsx
+++ src/__test__/wordle.test.tsx
@@ -12,29 +12,56 @@ describe("Home", () => {
   it("should render key in corret place", () => {
     const { getByTestId, queryByText } = render(<Wordle />);
 
+    fireEvent.keyDown(window, { key: "j", code: "KeyJ" });
+    fireEvent.keyDown(window, { key: "o", code: "KeyO" });
+    fireEvent.keyDown(window, { key: "g", code: "KeyG" });
     fireEvent.keyDown(window, { key: "a", code: "KeyA" });
-    fireEvent.keyDown(window, { key: "b", code: "KeyB" });
-    fireEvent.keyDown(window, { key: "c", code: "KeyC" });
-    fireEvent.keyDown(window, { key: "d", code: "KeyD" });
-    fireEvent.keyDown(window, { key: "e", code: "KeyE" });
-    fireEvent.keyDown(window, { key: "f", code: "KeyF" });
+    fireEvent.keyDown(window, { key: "r", code: "KeyR" });
+    fireEvent.keyDown(window, { key: "x", code: "KeyX" });
 
-    expect(getByTestId("word_0_0")).toHaveTextContent("a");
-    expect(getByTestId("word_0_1")).toHaveTextContent("b");
-    expect(getByTestId("word_0_2")).toHaveTextContent("c");
+    expect(getByTestId("word_0_0")).toHaveTextContent("j");
+    expect(getByTestId("word_0_1")).toHaveTextContent("o");
+    expect(getByTestId("word_0_2")).toHaveTextContent("g");
     expect(queryByText(/f/i)).not.toBeInTheDocument();
   });
 
   it("should only render 5 letters per line", () => {
     const { queryByText } = render(<Wordle />);
 
+    fireEvent.keyDown(window, { key: "j", code: "KeyJ" });
+    fireEvent.keyDown(window, { key: "o", code: "KeyO" });
+    fireEvent.keyDown(window, { key: "g", code: "KeyG" });
     fireEvent.keyDown(window, { key: "a", code: "KeyA" });
-    fireEvent.keyDown(window, { key: "b", code: "KeyB" });
-    fireEvent.keyDown(window, { key: "c", code: "KeyC" });
-    fireEvent.keyDown(window, { key: "d", code: "KeyD" });
-    fireEvent.keyDown(window, { key: "e", code: "KeyE" });
-    fireEvent.keyDown(window, { key: "f", code: "KeyF" });
+    fireEvent.keyDown(window, { key: "r", code: "KeyR" });
+    fireEvent.keyDown(window, { key: "x", code: "KeyX" });
 
     expect(queryByText(/f/i)).not.toBeInTheDocument();
+  });
+
+  it("should remove a letter when press Backspace", () => {
+    const { queryByTestId } = render(<Wordle />);
+
+    fireEvent.keyDown(window, { key: "j", code: "KeyJ" });
+    fireEvent.keyDown(window, { key: "o", code: "KeyO" });
+    fireEvent.keyDown(window, { key: "g", code: "KeyG" });
+    fireEvent.keyDown(window, { key: "a", code: "KeyA" });
+    fireEvent.keyDown(window, { key: "r", code: "KeyR" });
+    fireEvent.keyDown(window, { key: "Backspace", code: "Backspace" });
+
+    expect(queryByTestId("word_0_4")).not.toHaveTextContent("r");
+  });
+
+  it("should add letter to next line when press Enter", () => {
+    const { queryByTestId } = render(<Wordle />);
+
+    fireEvent.keyDown(window, { key: "j", code: "KeyJ" });
+    fireEvent.keyDown(window, { key: "o", code: "KeyO" });
+    fireEvent.keyDown(window, { key: "g", code: "KeyG" });
+    fireEvent.keyDown(window, { key: "a", code: "KeyA" });
+    fireEvent.keyDown(window, { key: "r", code: "KeyR" });
+    fireEvent.keyDown(window, { key: "Enter", code: "Enter" });
+    fireEvent.keyDown(window, { key: "x", code: "KeyX" });
+
+    expect(queryByTestId("word_1_0")).toHaveTextContent("x");
   });
 });
